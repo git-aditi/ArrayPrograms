76244a149f4f9aaada160fd4bc556f55ca52acd7
Aditi Thakkar <aditithakkar1998@gmail.com>
2022-04-05 13:50:11 -0400

    First Commit

---
+++ ArrayPrograms/src/main/java/com/company/java/CountArray.java
@@ -0,0 +1,16 @@
+package com.company.java;
+
+public class CountArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Initialize array
+		int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+		// Number of elements present in an array can be found using the length
+		System.out.println("Number of elements present in given array is " + arr.length);
+
+	}
+
+}
---
+++ ArrayPrograms/src/main/java/com/company/java/DuplicateElement.java
@@ -0,0 +1,23 @@
+package com.company.java;
+
+public class DuplicateElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//initialize array
+		int[] arr = new int[] {1,2,3,4,2,7,8,8,3};
+		System.out.println("Duplicate elements in given array: ");
+		
+		//search for duplicate elements
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					System.out.println(arr[j]);
+				}
+			}
+		}
+
+	}
+
+}
